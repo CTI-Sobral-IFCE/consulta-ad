cf5ca06986649bd6cd8f04e8443189d1eae9116c
Alan Freire <alan.freire.87@gmail.com>
2022-10-31 11:04:40 -0300

    Fix routes

--- routes/web.php
+++ routes/web.php
@@ -15,4 +15,4 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [StudentController::class, 'index'])->name('student.index');
-Route::post('/', [StudentController::class, 'search'])->name('student.search');
+Route::post('/resultado', [StudentController::class, 'search'])->name('student.search');
